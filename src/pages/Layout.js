c692f0a993c22ad9bc00500795347752a4595505
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-05-04 15:07:36 +0530

    Add Navbar component

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 function Layout() {
   return (
-    <div>Layout</div>
+    <>
+      <Navbar books="/" categories="/categories" />
+    </>
   );
 }
 
