cbb2ed9d1c8ac64d5e24b6f80675d817473e8c09
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-05-04 13:58:03 +0530

    Set up routers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout';
+import Books from './pages/Books';
+import Categories from './pages/Categories';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Book Store</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Books />} />
+          <Route path="categories" element={<Categories />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
