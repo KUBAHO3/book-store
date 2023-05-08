e2c756f2039eb42afe6e4ae851852c7432aa3fa4
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-05-08 16:25:58 +0530

    Create book slice

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { store } from './redux/store';
-import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
