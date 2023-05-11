e3d853ddeeaaa7b9522a0f841b0ad7f234348165
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-05-11 16:22:23 +0530

    Refine styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
 import App from './App';
 import store from './redux/store';
